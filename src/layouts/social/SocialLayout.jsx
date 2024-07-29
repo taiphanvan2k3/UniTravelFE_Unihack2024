2151263fa1b65120ae3316b4c4075197555e34ff
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-07-29 12:33:35 +0700

    fix: [Uni] Fix cannot access to New feed page

--- src/layouts/social/SocialLayout.jsx
+++ src/layouts/social/SocialLayout.jsx
@@ -4,12 +4,12 @@ import { Center, Container, Flex, Grid, GridItem, Image } from "@chakra-ui/react
 import { Link, Outlet } from "react-router-dom";
 import { logo } from "@/assets/images";
 import UserDropdown from "@/components/home/UserDropdown";
-import Cookies from "js-cookie";
-import { jwtDecode } from "jwt-decode";
+import { useContext } from "react";
+import { AuthContext } from "@/contexts/AuthContext";
 
 function SocialLayout() {
-    const accessToken = Cookies.get("access_token");
-    const decodedPayload = jwtDecode(accessToken);
+    const { auth } = useContext(AuthContext);
+
     return (
         <Container paddingX={"2rem"} maxW={"container.2xl"}>
             <Grid height={"100%"} width={"100%"} gap={6} templateColumns={"repeat(6, 1fr)"}>
@@ -18,7 +18,6 @@ function SocialLayout() {
                         <GridItem colSpan={1}>
                             <Center>
                                 <Link to="/">
-                                    {" "}
                                     <Image height={100} src={logo} marginRight={5} />
                                 </Link>
                             </Center>
@@ -27,7 +26,7 @@ function SocialLayout() {
                         <GridItem colSpan={1}>
                             {
                                 <Flex width={"100%"} height={"100%"} justifyContent={"center"} alignItems={"center"}>
-                                    <UserDropdown userInfo={decodedPayload} />
+                                    <UserDropdown userInfo={auth.user} />
                                 </Flex>
                             }
                         </GridItem>
--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,7 +1,9 @@
-import Cookies from "js-cookie";
+import { AuthContext } from "@/contexts/AuthContext";
+import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+
 const PrivateRoute = () => {
-    const accessToken = Cookies.get("access_token"); //
-    return accessToken ? <Outlet /> : <Navigate to="/auth/sign-in" />;
+    const { auth } = useContext(AuthContext);
+    return auth.user ? <Outlet /> : <Navigate to="/auth/sign-in" />;
 };
 export default PrivateRoute;
